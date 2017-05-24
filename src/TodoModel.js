d360a0583d77fba1f6571158a71d068d2d602b13
Chris Anderson <chris@fauna.com>
2017-05-23 19:54:16 -0700

    import queries from serverless-crud project

--- src/TodoModel.js
+++ src/TodoModel.js
@@ -1,8 +1,7 @@
 // const request = require('superagent');
 
-const todosEndpoint = process.env.NODE_ENV === "production" ?
-  'https://krmk0hfoo5.execute-api.us-east-1.amazonaws.com/prod'  :
-  'https://lb3gtcvf9f.execute-api.us-east-1.amazonaws.com/dev';
+const faunadb = require('faunadb');
+const q = faunadb.query;
 
 export default class TodoModel {
 	constructor (key) {
@@ -10,7 +9,7 @@ export default class TodoModel {
 		this.todos = [];
 		this.auth = {}
 		this.onChanges = [];
-		this.active = false;
+		this.active = false; // todo add observer to client
 	}
 	subscribe(onChange) {
 		this.onChanges.push(onChange);
@@ -38,121 +37,62 @@ export default class TodoModel {
 		this.active = is
 		this.inform(false)
 	}
-	makeRequest(id, verb, body) {
-		this.isActive(true);
-		return this.doMakeRequest(id, verb, body).then((ok)=>{
-			this.isActive(false);
-			return ok;
-		}).catch((e) => {
-			this.isActive('error');
-			throw e;
-		})
-	}
-	doMakeRequest(id, verb, body, count) {
-		if (typeof count === 'undefined') {
-			count = 2
-		}
-		if (count < 1) {
-			return Promise.reject("too many retries")
-		}
-		if (!this.auth.authorization_token) {
-			if (this.auth.doRefresh) {
-				return this.auth.doRefresh().then(()=> {
-					return this.makeRequest(id, verb, body, count -1)
-				});
-			} else {
-				return Promise.reject("no auth")
-			}
-		}
-		var path = todosEndpoint+"/todos";
-		if (id) {
-			path += "/" + id;
-		}
-		var options = {
-			method : verb,
-			headers : {
-				Authorization : this.auth.authorization_token,
-				'Content-Type' : 'application/json'
-			}
-		}
-		if (body) {
-			options.body = JSON.stringify(body)
-		}
-		console.log("makeRequest", options)
-		// var req = request(verb, path)
-		// 	.type('application/json')
-		// 	.set('Authorization', this.auth.authorization_token);
-		// if (body) {
-		// 	req.send(body)
-		// }
-		// return req
-
-		return fetch(path, options).then((ok)=>{
-			var contentType = ok.headers.get("content-type");
-  		if(contentType && contentType.indexOf("application/json") !== -1) {
-				return ok.json().then((data) => {
-					return data;
-				}).catch((e) => ok)
-			} else {
-				return ok;
-			}
-		})
-
-		.then((ok) => {
-			console.log("ok", ok)
-			return ok;
-		}).catch((e) => {
-			console.log('fetch error', e)
-			return this.auth.doRefresh().then(()=> {
-				return this.makeRequest(id, verb, body, count -1)
-			});
-		});
-	}
 	getServerTodos() {
-		return this.makeRequest(null, 'GET').then((r) => {this.todos = r.data;})
+		return client.query(
+      q.Map(
+        q.Paginate(q.Match(q.Ref("indexes/all_todos"))),
+        (ref) => q.Get(ref))).then((r) => {this.todos = r.data;})
 	}
 	addTodo(title) {
 		var newTodo = {
 			title: title,
 			completed: false
 		}
-		this.makeRequest(null, 'POST', newTodo).then((r) => {
+    const me = q.Select("ref", q.Get(q.Ref("classes/users/self")));
+    newTodo.user = me;
+    return client.query(q.Create(q.Ref("classes/todos"), {
+      newTodo,
+      permissions : {
+        read : me,
+        write : me
+      }
+     })).then((r) => {
 			this.inform()
 		})
 	}
 	toggleAll(checked) {
-		return this.makeRequest('toggle', 'POST').then((r) => {
-			this.inform();
-		})
+    return client.query(
+      q.Map(
+        q.Paginate(q.Match(q.Ref("indexes/all_todos"))),
+        (ref) => q.Update(q.Select("ref", q.Get(ref)),
+        { data : {
+          completed : q.Not(q.Select(["data", "completed"], q.Get(ref)))
+      }}))).then((r) => {
+			       this.inform();
+		 });
 	}
 	toggle(todoToToggle) {
-		var id = todoToToggle.ref["@ref"].split('/').pop();
 		console.log("todoToToggle", todoToToggle)
-		todoToToggle.data.completed = !todoToToggle.data.completed;
-		// return this.auth.doRefresh().then(()=> {
-		// 	return this.makeRequest(id, verb, body, count -1)
-		// });
-		return this.makeRequest(id, 'PUT', todoToToggle.data).then((r) => {
+    client.query(q.Update(todoToToggle.ref,
+        { data : {
+          completed : !todoToToggle.data.completed,
+        }})).then((r) => {
 			this.inform()
 		})
 	}
 	destroy(todo) {
-		var id = todo.ref["@ref"].split('/').pop();
-		this.makeRequest(id, 'DELETE').then((r) => {
-			this.inform()
-		})
+    return client.query(q.Delete(todo.ref)).then(() => this.inform());
 	}
 	save(todoToSave, text) {
-		var id = todoToSave.ref["@ref"].split('/').pop();
-		console.log("todoToSave", todoToSave)
-		todoToSave.data.title = text;
-		this.makeRequest(id, 'PUT', todoToSave.data).then((r) => {
-			this.inform()
-		})
+    return client.query(q.Update(todoToSave.ref),
+      {data : todoToSave.data}).then((r) => this.inform());
 	}
 	clearCompleted() {
-		return this.makeRequest('clear', 'POST').then((r) => {
-			this.inform();
-		})
+    return client.query(
+      q.Map(
+        q.Paginate(q.Match(q.Ref("indexes/all_todos"))),
+        (ref) => q.If(q.Select(["data", "completed"], q.Get(ref)),
+          q.Delete(q.Select("ref", q.Get(ref))),
+        true))).then((r) => this.inform());
 	}
 };
